d223bc4243b7293e9ed25dcf3a8928cfe89ad0e7
Moiz Mansoor Ali <moizmansooraliis@gmail.com>
2017-08-28 15:43:51 +0530

    Added the benchmark for large conversion in SAXTransformer

--- src/main/java/org/ballerina/state/Sources.java
+++ src/main/java/org/ballerina/state/Sources.java
@@ -22,7 +22,7 @@ public class Sources {
     @Setup(Level.Trial)
     public void setup() {
         try {
-            String xml = new String(Files.readAllBytes(Paths.get("xml-files/small.xml")));
+            String xml = new String(Files.readAllBytes(Paths.get("xml-files/large.xml")));
             String xsl = new String(Files.readAllBytes(Paths.get("xml-files/stylesheet.xml")));
             omXML = AXIOMUtil.stringToOM(xml);
             omXSL = AXIOMUtil.stringToOM(xsl);
